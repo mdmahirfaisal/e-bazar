d68e9bf96729ea9a737ebcfccfc756fc84000e11
MD Mahir Faisal <mahir.faisal@koneksys.com>
2024-10-17 23:07:47 +0600

    fixed firebase redirect issue

--- src/Firebase/Firebase.init.ts
+++ src/Firebase/Firebase.init.ts
@@ -5,13 +5,16 @@ import { getAnalytics, Analytics } from "firebase/analytics";
 import firebaseConfig from './Firebase.config';
 
 const initializeFirebase = (): FirebaseApp | void => {
-    console.log(firebaseConfig)
-    console.log(window)
-  // Initialize Firebase
-  const app: FirebaseApp = initializeApp(firebaseConfig);
+// Initialize Firebase
+//   const app: FirebaseApp = initializeApp(firebaseConfig);
+     initializeApp(firebaseConfig);
+  // Check if window object exists to prevent errors in non-browser environments
+//   if (typeof window !== 'undefined') {
+//     const analytics: Analytics = getAnalytics(app);
+//     return app;
+//   }
 
-    const analytics: Analytics = getAnalytics(app);
-    return app;
+//   return undefined;
 };
 
 export default initializeFirebase;
