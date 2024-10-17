4b5a0b9c6eb893292eba63a0e5cecb3c3d55b16d
MD Mahir Faisal <mahir.faisal@koneksys.com>
2024-10-17 22:46:37 +0600

    updated firebase.js file

--- src/Firebase/Firebase.init.ts
+++ src/Firebase/Firebase.init.ts
@@ -5,16 +5,13 @@ import { getAnalytics, Analytics } from "firebase/analytics";
 import firebaseConfig from './Firebase.config';
 
 const initializeFirebase = (): FirebaseApp | void => {
+    console.log(firebaseConfig)
+    console.log(window)
   // Initialize Firebase
   const app: FirebaseApp = initializeApp(firebaseConfig);
 
-  // Check if window object exists to prevent errors in non-browser environments
-  if (typeof window !== 'undefined') {
     const analytics: Analytics = getAnalytics(app);
     return app;
-  }
-
-  return undefined;
 };
 
 export default initializeFirebase;
